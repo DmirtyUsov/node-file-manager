ca830366b04979acc539aea886f0ea0eca0136d7
Dima Usov <dmitry.usov@yahoo.com>
2024-10-08 13:26:35 +0400

    feat: add commentator

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
+import { commentator } from "./src/commentator/index.js";
 console.clear();
+commentator.sayHi();
---
+++ src/commentator/commentator.js
@@ -0,0 +1,40 @@
+const DEFAULT_NAME = 'Incognito😎'
+class Commentator {
+
+  _userName = DEFAULT_NAME;
+
+  set userName(newName) {
+    this._userName = newName || DEFAULT_NAME;
+  }
+
+  say(message, useFn){
+    useFn(message);
+  }
+
+  sayHi() {
+    const message = `🖐 Welcome to the File Manager, ${this._userName}!`;
+    this.say(message, console.info);
+  }
+
+  sayBye() {
+    const message = `Thank you for using File Manager, ${this._userName}, goodbye! 👋`;
+    this.say(message, console.info);
+  }
+
+  sayCurrentDir() {
+    const message = `You are currently in \x1b[32m${process.cwd()} 📁\x1b[0m`;
+    this.say(message, console.info);
+  }
+
+  sayInvalidInput() {
+    const message = `❗ Invalid input`;
+    this.say(message, console.error);
+  }
+
+  sayOperationFailed() {
+    const message = `❌ Operation failed`;
+    this.say(message, console.error);
+  }
+}
+
+export const commentator = new Commentator();
