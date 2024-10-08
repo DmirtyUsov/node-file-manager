f94834aecce33d121e5e21451acd237077c42892
Dima Usov <dmitry.usov@yahoo.com>
2024-10-08 16:16:23 +0400

    feat: add FileSystemController.setHomeDir()

---
+++ src/file-system.controller.js
@@ -0,0 +1,19 @@
+import { homedir } from 'node:os';
+
+class FileSystemController {
+
+  changeDir(directory) {
+    try {
+      process.chdir(directory);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  setHomeDir() {
+    this.changeDir(homedir());
+  }
+}
+
+export const fileSystemController = new FileSystemController();
