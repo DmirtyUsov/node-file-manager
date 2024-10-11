71fcbd31a485a1f7d71d305a5e0b64863c010a24
Dima Usov <dmitry.usov@yahoo.com>
2024-10-11 11:14:10 +0400

    feat: add getContent for ls

--- src/file-system.commands.js
+++ src/file-system.commands.js
@@ -1,4 +1,5 @@
 import { homedir } from 'node:os';
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import { CmdAnswer } from './cmd-answer.model.js';
 
@@ -10,6 +11,27 @@ const changeDir = (directory) => {
     console.log(err);
     return false;
   }
+};
+
+const getDirEntryType = (dirEntry) => {
+  if (dirEntry.isFile()) {
+    return 'file';
+  }
+  if (dirEntry.isDirectory()) {
+    return 'directory';
+  }
+  return 'other';
+};
+
+const compareDirEntries = (entryA, entryB) => {
+  const isBothDirectories = entryA.isDirectory() && entryB.isDirectory();
+  const isBothFiles = entryA.isFile() && entryB.isFile();
+
+  if (isBothFiles || isBothDirectories) {
+    return entryA.name.localeCompare(entryB.name);
+  }
+
+  return entryA.isDirectory() ? -1 : 1;
 };
 
 export const moveToHomeDir = () => {
@@ -28,4 +50,25 @@ export const moveToParentDir = () => {
   const result = changeDir(path.resolve(getCurrDirAnswer.text, '../'));
 
   return new CmdAnswer(result);
+};
+
+export const getContent = async () => {
+  const path = process.cwd();
+  const answer = new CmdAnswer();
+
+  try {
+    const entries = await fs.readdir(path, { withFileTypes: true });
+    answer.tabularResult = entries
+      .filter((entry) => entry.isDirectory() || entry.isFile())
+      .sort(compareDirEntries)
+      .map((entry) => ({
+        Name: entry.name,
+        Type: getDirEntryType(entry),
+      }));
+  } catch {
+    return answer;
+  }
+
+  answer.isOk = true;
+  return answer;
 };
