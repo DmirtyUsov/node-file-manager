9e85a0dca3a8052344b71b33bf552f47c9b79cde
Dima Usov <dmitry.usov@yahoo.com>
2024-10-13 22:28:22 +0400

    feat: add quotes for input params

--- src/input.parsers.js
+++ src/input.parsers.js
@@ -4,7 +4,29 @@ export const getUserNameFromProcessArgv = () => {
 };
 
 export const getCmdAndArgsFromUserInput = (inputText) => {
-  const [cmd, ...args] = inputText.trim().split(' ');
-  const argsQuotesFree = args.map(arg => arg.replace(/^["'](.+(?=["']$))["']$/, '$1'));
-  return { cmd, args: argsQuotesFree };
+
+  const [cmd, ...args] = inputText
+    .trim()
+    .split('')
+    .reduce(
+      (accumulator, char) => {
+        let charToAdd = char;
+        if (char === '"') {
+          accumulator.isBeforeWasQuote = !accumulator.isBeforeWasQuote;
+          charToAdd = '';
+        }
+        if (char === ' ' && !accumulator.isBeforeWasQuote) {
+          charToAdd = '';
+          accumulator.splits.push([]);
+        }
+        if (charToAdd) {
+          const splitIdx = accumulator.splits.length - 1;
+          accumulator.splits[splitIdx].push(char);
+        }
+        return accumulator;
+      },
+      { splits: [[]], isBeforeWasQuote: false }
+    )
+    .splits.map((split) => split.join(''));
+  return { cmd, args };
 };
